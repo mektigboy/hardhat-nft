050424e20bd4205e8bdc58fe7e107cab7ae40d98
mektigboy <mektigboy@protonmail.com>
2022-08-03 13:52:12 -0500

    Create & update deploy/01-deploy-basic-nft.js

---
+++ deploy/01-deploy-basic-nft.js
@@ -0,0 +1,26 @@
+const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+module.exports = async function ({ deployments, getNamedAccounts }) {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    log("--------------------------------------------------");
+
+    const args = [];
+    const basicNFT = await deploy("BasicNFT", {
+        args: args,
+        from: deployer,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        log("Verifying...");
+        await verify(basicNFT.address, args);
+    }
+};
