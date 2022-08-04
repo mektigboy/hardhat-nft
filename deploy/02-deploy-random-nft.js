e876e00a1e92f533f41bdfdd4796d1c3b1fc72ed
mektigboy <mektigboy@protonmail.com>
2022-08-04 05:46:59 -0500

    Update deploy/02-deploy-random-nft.js

--- deploy/02-deploy-random-nft.js
+++ deploy/02-deploy-random-nft.js
@@ -1,14 +1,9 @@
 const { network, ethers } = require("hardhat");
-const { networkConfig } = require("../helper-hardhat-config");
+const {
+    developmentChains,
+    networkConfig,
+} = require("../helper-hardhat-config");
 const { verify } = require("../utils/verify");
-
-const tokenURIs = [
-    "ipfs://bafkreidiszt2rp5unghfq3xfdagqcl7b6z2yc4ef6bmkbqbrqrcvoahnby", // 1
-    "ipfs://bafkreiaulh6ope6bamhyhlzgwisc3djffjuclt5zxxamsypmqrulb3kkqa", // 2
-    "ipfs://bafkreiayes7ej5kzziio3fydpjjtdygtaxckjitka5z6txfgqgkfeticaq", // 3
-];
-
-const FUND_AMOUNT = "10000000000000000000"; // 10 LINK
 
 module.exports = async function ({ deployments, getNamedAccounts }) {
     const { deploy, log } = deployments;
@@ -17,13 +12,21 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
 
     let coordinatorAddress, subscriptionId;
 
+    const FUND_AMOUNT = "1000000000000000000000";
+
+    const tokenURIs = [
+        "ipfs://bafkreidiszt2rp5unghfq3xfdagqcl7b6z2yc4ef6bmkbqbrqrcvoahnby", // 1
+        "ipfs://bafkreiaulh6ope6bamhyhlzgwisc3djffjuclt5zxxamsypmqrulb3kkqa", // 2
+        "ipfs://bafkreiayes7ej5kzziio3fydpjjtdygtaxckjitka5z6txfgqgkfeticaq", // 3
+    ];
+
     if (chainId == 31337) {
         const coordinatorMock = await ethers.getContract(
             "VRFCoordinatorV2Mock"
         );
         coordinatorAddress = coordinatorMock.address;
-        const transaction = await coordinatorMock.createSubscription();
-        const transactionReceipt = await transaction.wait(1);
+        const transactionResponse = await coordinatorMock.createSubscription();
+        const transactionReceipt = await transactionResponse.wait(1);
         subscriptionId = transactionReceipt.events[0].args.subId;
         await coordinatorMock.fundSubscription(subscriptionId, FUND_AMOUNT);
     } else {
@@ -33,17 +36,17 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
 
     log("--------------------------------------------------");
 
-    const args = [
+    const arguments = [
         coordinatorAddress,
         subscriptionId,
-        networkConfig[chainId].gasLane,
-        networkConfig[chainId].callbackGasLimit,
+        networkConfig[chainId]["gasLane"],
+        networkConfig[chainId]["callbackGasLimit"],
         tokenURIs,
-        networkConfig[chainId].mintFee,
+        networkConfig[chainId]["mintFee"],
     ];
 
     const randomNFT = await deploy("RandomNFT", {
-        args: args,
+        args: arguments,
         from: deployer,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
@@ -51,9 +54,12 @@ module.exports = async function ({ deployments, getNamedAccounts }) {
 
     log("--------------------------------------------------");
 
-    if (!chainId == 31337 && process.env.ETHERSCAN_API_KEY) {
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
         log("Verifying...");
-        await verify(randomNFT.address, args);
+        await verify(randomNFT.address, arguments);
     }
 };
 
